25ae72337584b2af13e98ac808328be1f81384fd
najmus-sakib-hossain <manfromexistence@proton.me>
2025-11-05 15:32:33 +0600

    fix: update favicon links to use specific sizes and formats

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,8 +32,8 @@
 
         <title inertia>{{ config('app.name', 'Notes Of Shahrier') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png">
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
